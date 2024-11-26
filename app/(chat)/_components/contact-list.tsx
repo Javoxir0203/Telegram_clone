7d926b1b7c7d5eadc1610dc4221ca88bd55e791b
Eshquvvatov Javoxir <javoxireshquvvatovv@gmail.com>
2024-11-26 10:33:19 +0500

    active

--- app/(chat)/_components/contact-list.tsx
+++ app/(chat)/_components/contact-list.tsx
@@ -1,28 +1,48 @@
+'use client'
 import { IUser } from '@/types'
 import { FC, Fragment } from 'react'
 import Settings from './settings'
 import { Input } from '@/components/ui/input'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { useRouter } from 'next/navigation'
+import useCurrentContact from '@/hooks/use-current'
+import { cn } from '@/lib/utils'
 interface Props {
 	contacts: IUser[]
 }
 const ContactList: FC<Props> = ({ contacts }) => {
+	const router = useRouter()
+	const { setCurrentContact, currentContact } = useCurrentContact()
 	const renderContact = (contact: IUser) => {
+		const onChat = () => {
+			if (currentContact?._id === contact._id)return
+			console.log('chatting with', contact.email)
+				setCurrentContact(contact)
+			router.push(`/?chat=${contact._id}`)
+		}
 		return (
-			<div className='flex justify-between items-center cursor-pointer hover:bg-secondary/50 p-2'>
+			<div className={cn('flex justify-between items-center cursor-pointer hover:bg-secondary/50 p-2',currentContact?._id===contact._id && 'bg-secondary/50')} onClick={onChat}>
 				<div className='flex items-center gap-2'>
 					<div className='relative'>
 						<Avatar className='z-40'>
 							<AvatarImage src={contact.avatar} alt={contact.email} className='object-cover' />
 							<AvatarFallback className='uppercase'>{contact.email[0]}</AvatarFallback>
 						</Avatar>
+						<div className=' size-3 bg-green-500 absolute rounded-full bottom-0 right-0 !z-50' />
 					</div>
+					<div>
+						<h2 className=' capitalize line-clamp-1 text-sm'>{contact.email.split('@')[0]}</h2>
+						<p className='text-xs line-clamp-1 text-muted-foreground'>No message yet </p>
+					</div>
+				</div>
+				<div className='self-end'>
+					<p className='text-xs text-muted-foreground'>19:20 pm</p>
 				</div>
 			</div>
 		)
 	}
 	return (
-		<Fragment>
+		<>
 			{/* Top bar */}
 			<div className='flex items-center bg-background pl-2 sticky top-0'>
 				<Settings />
@@ -38,8 +58,10 @@ const ContactList: FC<Props> = ({ contacts }) => {
 				</div>
 			)}
 
-			{contacts.map(contact => renderContact(contact))}
-		</Fragment>
+			{contacts.map(contact => (
+				<div key={contact._id}>{renderContact(contact)}</div>
+			))}
+		</>
 	)
 }
 
