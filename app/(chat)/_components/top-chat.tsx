c57b717d2c6c6713b7cf2347cd3eb2c2af820fa4
Eshquvvatov Javoxir <javoxireshquvvatovv@gmail.com>
2024-11-29 15:18:21 +0500

    next pages

---
+++ app/(chat)/_components/top-chat.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TopChat = () => {
+	return <div>Topchat</div>
+}
+
+export default TopChat
--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -6,14 +6,29 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import AddContact from './_components/add.contact'
 import { useCurrentContact } from '@/hooks/use-current'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { emailSchema } from '@/lib/validation'
+import { zodResolver } from '@hookform/resolvers/zod'
+import TopChat from './_components/top-chat'
 
 const Home = () => {
 	const { currentContact } = useCurrentContact()
 	const router = useRouter()
-
+	//
+	const contactForm = useForm<z.infer<typeof emailSchema>>({
+		resolver: zodResolver(emailSchema),
+		defaultValues: { email: '' },
+	})
+	//
 	useEffect(() => {
 		router.replace('/')
 	}, [])
+
+	const onCreateContact = (values: z.infer<typeof emailSchema>) => {
+		// API call to create contact
+		console.log(values)
+	}
 	return (
 		<>
 			{/* Saidber */}
@@ -29,9 +44,13 @@ const Home = () => {
 			{/* Chat area */}
 			<div className='pl-80 w-full'>
 				{/* Add contact */}
-				{!currentContact?._id && <AddContact />}
+				{!currentContact?._id && <AddContact contactForm={contactForm} onCreateContact={onCreateContact} />}
 				{/* chat */}
-				{currentContact?._id && <div>Chat</div>}
+				{currentContact?._id && <div className='w-full relative'>
+					{/* Top chat */}
+					<TopChat/>
+					{/* chat message */}
+					</div>}
 			</div>
 		</>
 	)
