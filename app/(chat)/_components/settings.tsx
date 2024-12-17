248ce51fdf99a8ad73dd0f140568d6f8dcf51618
Eshquvvatov Javoxir <javoxireshquvvatovv@gmail.com>
2024-12-17 23:54:32 +0500

    next

--- app/(chat)/_components/settings.tsx
+++ app/(chat)/_components/settings.tsx
@@ -1,9 +1,12 @@
 import { Button } from '@/components/ui/button'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { Separator } from '@/components/ui/separator'
-import { Menu, Settings2, UserPlus } from 'lucide-react'
+import { Switch } from '@/components/ui/switch'
+import { Menu, Moon, Settings2, Sun, UserPlus, VolumeOff } from 'lucide-react'
+import { useTheme } from 'next-themes'
 
 const Settings = () => {
+	const {resolvedTheme, setTheme} = useTheme()
 	return (
 		<>
 			<Popover>
@@ -21,16 +24,32 @@ const Settings = () => {
 						<div className='flex items-center justify-between p-2 hover:bg-secondary cursor-pointer'>
 							<div className='flex items-center gap-1'>
 								<Settings2 size={16} />
-                <span className='text-sm'>Profile</span>
+								<span className='text-sm'>Profile</span>
 							</div>
 						</div>
 
-            <div className='flex items-center justify-between p-2 hover:bg-secondary cursor-pointer'>
-              <div className='flex items-center gap-1'>
-                <UserPlus size={16} />
-                <span className='text-sm'>Create contact</span>
-              </div>
-            </div>
+						<div className='flex items-center justify-between p-2 hover:bg-secondary cursor-pointer'>
+							<div className='flex items-center gap-1'>
+								<UserPlus size={16} />
+								<span className='text-sm'>Create contact</span>
+							</div>
+						</div>
+
+						<div className='flex items-center justify-between p-2 hover:bg-secondary '>
+							<div className='flex items-center gap-1'>
+								<VolumeOff size={16} />
+								<span className='text-sm'>Mute</span>
+							</div>
+							<Switch />
+						</div>
+
+						<div className='flex items-center justify-between p-2 hover:bg-secondary '>
+							<div className='flex items-center gap-1'>
+								{resolvedTheme === 'dark' ? <Sun size={16} /> : <Moon size={16} />}
+								<span className='text-sm'>{resolvedTheme === 'dark' ? 'Light mode' : 'Dark mode'}</span>
+							</div>
+							<Switch  checked={resolvedTheme === 'dark' ? true: false } onCheckedChange={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}/>
+						</div>
 					</div>
 				</PopoverContent>
 			</Popover>
